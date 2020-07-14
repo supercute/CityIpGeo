f478741a9c9d2dbdfe06b927f7e389a145b762db
supercute <d.shumskikh@gmail.com>
2020-07-14 14:28:06 +0300

    Add file_exist check

--- cipg.php
+++ cipg.php
@@ -26,43 +26,39 @@ if (isset($_GET['upload_db']) && $_GET['upload_db'] == 'y') {
     }
     
 }
+if (file_exists($path)) {
+    $ip = new RemoteAddress();
+    try {
+        $ipGeoBase = new IpGeoBase($path);
+    } catch (Exception $e) {
+        echo "Ошибка создания обьекта класса ipGeoBase";
+    }
 
-$ip = new RemoteAddress();
+    $info =  $ipGeoBase->search('94.181.214.151');
+    $locations = $ipGeoBase->listLocations();
 
-try {
-    $ipGeoBase = new IpGeoBase($path);
-} catch (Exception $e) {
-    echo "Ошибка создания обьекта класса ipGeoBase";
-}
-$info =  $ipGeoBase->search('94.181.214.151');
-$locations = $ipGeoBase->listLocations();
-//var_dump($locations);
-
-if (isset($_POST['query'])) {
-    $query = $_POST['query'];
-    foreach ($locations as $location) {
-        if (mb_stripos($location['city'], $query,0,'UTF-8') !== false) {
-            $searchLocations[] = $location;
+    if (isset($_POST['query'])) {
+        $query = $_POST['query'];
+        foreach ($locations as $location) {
+            if (mb_stripos($location['city'], $query,0,'UTF-8') !== false) {
+                $searchLocations[] = $location;
+            }
         }
-    }
-    header('Content-type: application/json');
-    if (!empty($searchLocations)) {
-        echo json_encode($searchLocations);
+        header('Content-type: application/json');
+        if (!empty($searchLocations)) {
+            echo json_encode($searchLocations);
+        } else {
+            echo 'Не найдено результатов';
+        }
+    } else if (isset($_POST['selected_city'])) {
+        setcookie("CIPG_CITY", $_POST['selected_city']);
+        return true;
     } else {
-        echo 'Не найдено результатов';
+        if (!isset($_COOKIE['CIPG_CITY'])) {
+            setcookie("CIPG_CITY", $info['city']);
+        }
+        echo $_COOKIE['CIPG_CITY'];
     }
-} else if (isset($_POST['selected_city'])) {
-    setcookie("CIPG_CITY", $_POST['selected_city']);
-    return true;
 } else {
-    if (!isset($_COOKIE['CIPG_CITY'])) {
-        setcookie("CIPG_CITY", $info['city']);
-    }
-    echo $_COOKIE['CIPG_CITY'];
+    echo "Не найдена база городов, загрузите /cipg.php?upload_db=y";
 }
-
-
-//Выводит список всех городов
-//$cities = $ipGeoBase->listCity();
-//
-//print_r($cities);
